ebe6a6887685d8752914cefe9a8dbace558da3fa
Anton <menchacanton@gmail.com>
2021-11-08 22:06:04 -0500

    TDD write Green passing tests for Movie Buttons

---
+++ src/components/Movie/MovieBtn.vue
@@ -0,0 +1,13 @@
+<template>
+    <button v-if="isFavorited">Remove Favorite</button>   
+    <button v-else>Add Favorite</button>   
+</template>
+
+<script>
+export default {
+    name: "MovieBtn",
+    props: {
+    isFavorited: Boolean,
+  },
+}
+</script>
