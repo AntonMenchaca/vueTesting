fba2a6de4f0952bb611dd209d42a8b69c03b04b7
Anton <menchacanton@gmail.com>
2021-11-12 17:28:07 -0500

    edit env variables

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = "mongodb://localhost:27017/testingwithjest";
+const url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0.ajay8.mongodb.net/test`;
 mongoose.Promise = Promise;
 
 mongoose
